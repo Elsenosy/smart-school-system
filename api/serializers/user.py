b44bb08c1d85caf47e9b46c2995298f88dfc5d07
Taha Elsenosy <tahaelsenosy@gmail.com>
2020-05-01 13:39:36 +0200

    Modifying User model to selt hire_date, job as Nullable

--- api/serializers/user.py
+++ api/serializers/user.py
@@ -10,6 +10,12 @@ class UserSerializer(serializers.ModelSerializer):
 
     # Specific code for creating user, to generate token for each user
     def create(self, validated_data):
+        picture     = validated_data['picture']
+        hire_date   = validated_data['hire_date']
+        job         = validated_data['job']
+        stage       = validated_data['stage']
+        category    = validated_data['category']
+        
         user = User(
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
@@ -18,7 +24,12 @@ class UserSerializer(serializers.ModelSerializer):
             user_type=validated_data['user_type'],
             address=validated_data['address'],
             phone=validated_data['phone'],
-            birth_date=validated_data['birth_date']
+            birth_date=validated_data['birth_date'],
+            picture=picture,
+            hire_date=hire_date,
+            job=job,
+            stage=stage,
+            category=category,        
             )
         user.set_password(validated_data['password'])
         user.save()
