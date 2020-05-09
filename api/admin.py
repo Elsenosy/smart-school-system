ebe5359ed7f004d844834e1125147e536f5559e2
Mustafa Hesham <50930361+mustafa-hesham@users.noreply.github.com>
2020-05-09 15:59:38 +0200

    Add files via upload

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import User, Category, Stage, Questionnair, Qanswer, Subject
+from api.models.material import Material
 
 # Register your models here.
 admin.site.register(User)
@@ -8,3 +9,4 @@ admin.site.register(Category)
 admin.site.register(Questionnair)
 admin.site.register(Qanswer)
 admin.site.register(Subject)
+admin.site.register(Material)
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .viewsets import UserViewSet, UserCreate, \
+from api.viewsets import UserViewSet, UserCreate, \
                 StageViewSet, CategoryViewSet, \
                 SubjectViewSet, QuestionnairViewSet, \
                 QanswerViewSet
+from api.viewsets.material import MaterialViewSet
 
 from .apiviews import LoginView, UserSubjects
 
@@ -17,6 +18,7 @@ router.register('categorys',     CategoryViewSet)
 router.register('subjects',      SubjectViewSet)
 router.register('questionnairs', QuestionnairViewSet)
 router.register('qanswers',      QanswerViewSet)
+router.register('material',      MaterialViewSet)
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
