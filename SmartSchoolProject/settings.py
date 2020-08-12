2f8e2dbde59cb8d909d28aaacda317d2ad0b2b1f
Taha Elsenosy <tahaelsenosy@gmail.com>
2020-08-12 23:37:57 +0200

    Add media directory

--- SmartSchoolProject/settings.py
+++ SmartSchoolProject/settings.py
@@ -142,8 +142,10 @@ AUTH_USER_MODEL = 'api.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, "SmartSchoolProject", "media")
 STATICFILES_DIR = [
     os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR, "SmartSchoolProject", "media"),
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
